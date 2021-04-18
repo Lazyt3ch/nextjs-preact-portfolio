bd661198b398850a66eee834ea0bb96d444a5230
Rashid <Lazyt3ch@gmail.com>
2021-04-18 09:46:58 +0500

    fix styles import in projects/[id].js

--- src/pages/projects/[id].js
+++ src/pages/projects/[id].js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import styles from '../styles/Project.module.css'
+import styles from '../../styles/Project.module.css';
 
 import { useRouter } from 'next/router';
 const { JSDOM } = require('jsdom');
