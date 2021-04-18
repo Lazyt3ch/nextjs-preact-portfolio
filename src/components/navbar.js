9e6c9c03d1f2c389b7904b3b16e771ed555f5e88
Rashid <Lazyt3ch@gmail.com>
2021-04-18 15:13:44 +0500

    create container.js and its styles; adjust styles

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,5 +1,14 @@
-import "../styles/Navbar.module.css";
+import styles from "../styles/Navbar.module.css";
+import Link from "next/link";
 
 export default function Navbar() {
-
+  return (
+    <div className={styles.navbar}>
+      <Link href="/">
+        <a className="external_link">
+          Home
+        </a>
+      </Link>
+    </div>
+  );
 };
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,16 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import Navbar from "../components/navbar";
+import Container from "../components/container";
 
-function MyApp({ Component, pageProps }) {
-  return (<>
-    <Component { ...pageProps } />
-  </>);
+function MyApp({ Component, pageProps }) {  
+  return (
+    <>
+      <Navbar />
+      <Container>
+        <Component { ...pageProps } />
+      </Container>
+    </>
+  );
 }
 
 export default MyApp
