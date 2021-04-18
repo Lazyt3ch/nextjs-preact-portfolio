5386e46caaa8384dbef558b922844b61904fa86c
Rashid <Lazyt3ch@gmail.com>
2021-04-18 09:31:23 +0500

    change projects/[id].js

--- src/pages/projects/[id].js
+++ src/pages/projects/[id].js
@@ -1,6 +1,21 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Project.module.css'
 const { JSDOM } = require('jsdom');
 
 // https://freelance.habr.com/freelancers/Lazytech/projects
 const baseUrl = "https://freelance.habr.com";
+
+export default function Project(props) {
+
+}
+
+
+export async function getStaticPaths() {
+  
+}
+
+
+export async function getStaticProps({ params }) {
+
+
+}
