df4a057d14a7a5037cede13492b2aa23311b36a6
Rashid <Lazyt3ch@gmail.com>
2021-04-18 09:39:46 +0500

    change projects/[id].js

--- src/pages/projects/[id].js
+++ src/pages/projects/[id].js
@@ -1,21 +1,23 @@
 import Head from 'next/head'
 import styles from '../styles/Project.module.css'
+
+import { useRouter } from 'next/router';
 const { JSDOM } = require('jsdom');
 
 // https://freelance.habr.com/freelancers/Lazytech/projects
 const baseUrl = "https://freelance.habr.com";
 
 export default function Project(props) {
+  const router = useRouter();
+  const { id } = router.query;
 
-}
-
-
-export async function getStaticPaths() {
-  
-}
-
-
-export async function getStaticProps({ params }) {
+  return (
+    <div>
+      Post: {id}
+    </div>
+  );
 
 
 }
+
+
