a3a291ef5c491b3ba7ef8180aeeadd97f0c1b8a5
Rashid <Lazyt3ch@gmail.com>
2021-04-18 09:25:39 +0500

    add projects/[id].js

---
+++ src/pages/projects/[id].js
@@ -0,0 +1,6 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+const { JSDOM } = require('jsdom');
+
+// https://freelance.habr.com/freelancers/Lazytech/projects
+const baseUrl = "https://freelance.habr.com";
