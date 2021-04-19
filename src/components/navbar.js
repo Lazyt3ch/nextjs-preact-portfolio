72cefc70766a9c8d690d779aad0eec4905ecc750
Rashid <Lazyt3ch@gmail.com>
2021-04-19 13:53:55 +0500

    make links in Navbar conditional

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,15 +1,26 @@
 import styles from "../styles/Navbar.module.css";
 import Link from "next/link";
+import { useRouter } from 'next/router';
 
 export default function Navbar() {
+  const router = useRouter();
+  const { pathname } = router;
+  console.log(pathname);
+
   return (
     <div className={styles.navbar}>
       <Link href="/">
-        <a className="external_link">На главную</a>
+        { pathname === "/"
+          ? <span>На главную</span>
+          : <a className="external_link">На главную</a>
+        }        
       </Link>
-      
+
       <Link href="/about">
-        <a className="external_link">О проекте</a>
+        { pathname === "/about"
+          ? <span>О проекте</span>
+          : <a className="external_link">О проекте</a>
+        }        
       </Link>
     </div>
   );
