f11305a3de92cb1dcf3ac493d470c9b88b70f9d6
Rashid <Lazyt3ch@gmail.com>
2021-04-19 09:41:46 +0500

    create about.js and styles file for it

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+import styles from '../styles/About.module.css';
+
+import Link from "next/link";
+
+export default function About() {
+  return (
+    <>
+      <Head />
+      <div>
+
+      </div>
+    </>
+  );
+}
