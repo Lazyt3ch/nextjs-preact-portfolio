cef67a9278ef0d0dbe9e0dd90e15df94c8f4ac0f
Rashid <Lazyt3ch@gmail.com>
2021-04-19 10:14:25 +0500

    adjust Container and About styles; add text in About

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,9 +7,14 @@ export default function About() {
   return (
     <>
       <Head />
-      
-      <div>
-        About Project
+
+      <div className={styles.container}>
+        <h1 className={styles.pageTitle}>
+          Техническая информация о веб-приложении
+        </h1>
+        <div className={styles.description}>
+          Данное приложение сделано на фреймворке <a href="https://nextjs.org/" className="external_link">Next.js</a>, основанном на библиотеке <a href="https://reactjs.org/" className="external_link">React</a>.
+        </div>
       </div>
     </>
   );
